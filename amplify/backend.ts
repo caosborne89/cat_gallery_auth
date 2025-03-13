5fe34d41e2de1523944f81c4f0f9e4a3dd6fcb68
Andy Osborne <cao89@arizona.edu>
2025-03-13 13:53:45 -0700

    Update

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -17,6 +17,7 @@ const backend = defineBackend({
 const s3Bucket = backend.storage.resources.bucket;
 const cfnBucket = s3Bucket.node.defaultChild as s3.CfnBucket;
 
+
 // Enable static website hosting
 cfnBucket.websiteConfiguration = {
   indexDocument: "index.html",
@@ -25,12 +26,13 @@ cfnBucket.websiteConfiguration = {
 
 // // Grant public read access by updating the bucket policy
 // const bucketPolicy = new iam.PolicyStatement({
+//   effect: iam.Effect.ALLOW,
 //   actions: ["s3:GetObject"],
 //   resources: [`${s3Bucket.bucketArn}/*`],
 //   principals: [new iam.AnyPrincipal()], // Allows public access
 // });
 
-// s3Bucket.addToResourcePolicy(bucketPolicy);
+
 
 // Remove default S3 public access blocks
 cfnBucket.publicAccessBlockConfiguration = {
@@ -39,3 +41,22 @@ cfnBucket.publicAccessBlockConfiguration = {
   ignorePublicAcls: false,
   restrictPublicBuckets: false,
 };
+
+// Define a completely new bucket policy
+const bucketPolicy = new s3.CfnBucketPolicy(s3Bucket, 'MyNewBucketPolicy', {
+  bucket: s3Bucket.bucketName,
+  policyDocument: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Principal: '*',
+        Action: ['s3:GetObject'],
+        Resource: `${s3Bucket.bucketArn}/*`,
+      },
+    ],
+  },
+});
+
+// Ensure the policy is applied after the bucket is created
+bucketPolicy.node.addDependency(cfnBucket);
